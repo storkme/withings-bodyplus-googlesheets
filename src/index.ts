4b8214246f8f7187343beb734cb63137962fc767
Alex Storkey <alex.storkey@superawesome.com>
2021-03-14 16:41:51 +0000

    subscribing for notifications

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,59 @@
 import "source-map-support/register";
 import express from "express";
 import pino from "pino-http";
-import * as read from './routes/read';
-import * as path from 'path';
-import fetch from 'node-fetch';
+import * as read from "./routes/read";
+import * as path from "path";
+import fetch from "node-fetch";
 
 const app = express();
 const logger = pino({ level: "debug" });
 
 if (!process.env.WITHINGS_CLIENT_ID || !process.env.WITHINGS_CLIENT_SECRET) {
-  console.error('Missing WITHINGS_CLIENT_ID/WITHINGS_CLIENT_SECRET');
+  console.error("Missing WITHINGS_CLIENT_ID/WITHINGS_CLIENT_SECRET");
   process.exit(1);
 }
 
 app.use(logger);
 
-app.get('/', express.static(path.join(__dirname, '../static')));
+app.get("/", express.static(path.join(__dirname, "../static")));
 app.post("/read", ...read.middleware, read.route());
 app.get("/oauth", async (req, res, next) => {
   try {
     // get access token as per https://developer.withings.com/oauth2/#operation/oauth2-authorize
-    const result = await fetch(`https://wbsapi.withings.net/v2/oauth2`, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-        action: 'requesttoken',
-        grant_type: 'access_token',
-        client_id: process.env.WITHINGS_CLIENT_ID!!,
-        client_secret: process.env.WITHINGS_CLIENT_SECRET!!,
-        code: req.query.code as string,
-        redirect_uri: `https://withings-bodyplus-googlesheets.not.gd/oauth`,
+    const authResult = await (
+      await fetch(`https://wbsapi.withings.net/v2/oauth2`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          action: "requesttoken",
+          grant_type: "access_token",
+          client_id: process.env.WITHINGS_CLIENT_ID!!,
+          client_secret: process.env.WITHINGS_CLIENT_SECRET!!,
+          code: req.query.code as string,
+          redirect_uri: `https://withings-bodyplus-googlesheets.not.gd/oauth`,
+        }),
       })
-    });
+    ).json();
 
-    res.status(200).json(await result.json());
+    const notificationResult = await (
+      await fetch("https://wbsapi.withings.net/notify", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded",
+          Authorization: `Bearer ${authResult.access_token}`,
+        },
+        body: new URLSearchParams({
+          action: "subscribe",
+          callback_url: `https://withings-bodyplus-googlesheets.not.gd/read`,
+        }),
+      })
+    ).json();
+
+    req.log?.child({ authResult, notificationResult }).info('subscribed?');
+
+    res.status(200).json(notificationResult);
   } catch (e) {
     next(e);
   }
